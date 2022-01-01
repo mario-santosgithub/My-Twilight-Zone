7260554bd787ad088a803b7816bc47659a637622
mario_santos <75098183+mario-santosgithub@users.noreply.github.com>
2022-01-01 20:18:52 +0000

    Create Comment.py

---
+++ Comment.py
@@ -0,0 +1,15 @@
+class Comment:
+
+    def __init__(self, user, stance, message):
+        self.user = user
+        self.stance = stance
+        self.message = message
+
+    def getUser(self):
+        return self.user
+
+    def getStance(self):
+        return self.stance
+
+    def getMessage(self):
+        return self.message
