e5c836aa70aabc586f5acdd12ec5f62c723b321b
mario_santos <75098183+mario-santosgithub@users.noreply.github.com>
2022-01-01 17:34:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from Strings import *
 from User import *
 from Fakebook import *
 from Post import *
-
+import operator
 
 fakebook = Fakebook()
 
-
-#Interpretador de comandos
 def main():
     line = input()
     arguments = line.split()
@@ -15,22 +13,11 @@ def main():
 
     while command != EXIT:
         if command == HELP:
-            print(REGISTER.lower() + " - " + MSG_REGISTER)
-            print(ADDFRIEND.lower() + " - " + MSG_ADDFRIEND)
-            print(FRIENDS.lower() + " - " + MSG_FRIENDS)
-            print(POST.lower() + " - " + MSG_POST)
-            print(USERPOSTS.lower() + " - " + MSG_USERPOSTS)
-            print(COMMENT.lower() + " - " + MSG_COMMENT)
-            print(READPOST.lower() + " - " + MSG_READPOST)
-            print(COMMENTSBYUSER.lower() + " - " + MSG_COMMENTSBYUSER)
-            print(TOPICFANATICS.lower() + " - " + MSG_TOPICFANATICS)
-            print(TOPICPOSTS.lower() + " - " + MSG_TOPICPOSTS)
-            print(HELP.lower() + " - " + MSG_HELP)
-            print(EXIT.lower() + " - " + MSG_EXIT)
+            help()
         elif command == REGISTER:
             registerUser(arguments)
         elif command == USERS:
-            users(arguments)
+            users()
         elif command == ADDFRIEND:
             addFriend(arguments)
         elif command == FRIENDS:
@@ -43,27 +30,39 @@ def main():
             comment(arguments)
 
         else:
-            print(MSG_UNKNOW)
-        
+            print(MSG_UNKNOWN)
+
+
         line = input()
         arguments = line.split()
         command = arguments[0].upper()
-    print(BYE_MSG)
-        
 
+    print(BYE_MSG)
+
+def help():
+    print(REGISTER.lower() + " - " + MSG_REGISTER)
+    print(ADDFRIEND.lower() + " - " + MSG_ADDFRIEND)
+    print(FRIENDS.lower() + " - " + MSG_FRIENDS)
+    print(POST.lower() + " - " + MSG_POST)
+    print(USERPOSTS.lower() + " - " + MSG_USERPOSTS)
+    print(COMMENT.lower() + " - " + MSG_COMMENT)
+    print(READPOST.lower() + " - " + MSG_READPOST)
+    print(COMMENTSBYUSER.lower() + " - " + MSG_COMMENTSBYUSER)
+    print(TOPICFANATICS.lower() + " - " + MSG_TOPICFANATICS)
+    print(TOPICPOSTS.lower() + " - " + MSG_TOPICPOSTS)
+    print(HELP.lower() + " - " + MSG_HELP)
+    print(EXIT.lower() + " - " + MSG_EXIT)
 
 def registerUser(arguments):
-    global fakebook
 
     userKind = arguments[1]
     if userKind.upper() not in ["NAIVE", "FANATIC", "SELFCENTERED"]:
         print(userKind.lower() + " is an invalid user kind!")
         return
-
-
+    
     userId = ""
-    for word in range(2, len(arguments)):
-        userId = userId + arguments[word] + " "
+    for i in range(2, len(arguments)):
+        userId += arguments[i] + " "
     userId = userId[0:-1]
 
     users = fakebook.getUsers()
@@ -72,33 +71,38 @@ def registerUser(arguments):
         if userId == user.getId():
             print(userId + " already exists!")
             return
-
-
-
+    
     if userKind.upper() == "FANATIC":
-        lovesHates = input()
-        lovesHates = lovesHates.split()
+        hashtags = input().split()
+        lovesHates = []
         loves = []
         hates = []
-        read = "loves"
-        for word in lovesHates:
-            if word == "loves":
-                read = "loves"
-            elif word == "hates":
-                read = "hates"
+        read = "LOVES"
+        for word in hashtags:
+            if word.upper() == "LOVES":
+                read = "LOVES"
+            elif word.upper() == "HATES":
+                read = "HATES"
             else:
-                if read == "loves":
+                lovesHates.append([read, word])
+                if read.upper() == "LOVES":
+                    if word in loves:
+                        print("Invalid fanaticism list!")
+                        return
                     loves.append(word)
                 else:
+                    if word in hates:
+                        print("Invalid fanaticism list!")
+                        return
                     hates.append(word)
 
         for i in loves:
             for j in hates:
-                if j == i:
+                if i == j:
                     print("Invalid fanaticism list!")
                     return
 
-        user = Fanatic(userId, userKind, loves, hates)
+        user = Fanatic(userId, userKind, lovesHates)
 
     elif userKind.upper() == "NAIVE":
         user = Naive(userId, userKind)
@@ -108,8 +112,9 @@ def registerUser(arguments):
     fakebook.addUser(user)
     print(userId + " registered.")
 
-def users(arguments):
+def users():
     users = fakebook.getUsers()
+    users.sort(key=operator.attrgetter("userId"))
     if users == []:
         print("There are no users!")
         return
@@ -129,11 +134,11 @@ def addFriend(arguments):
     users = fakebook.getUsers()
     
     userId1 = ""
-    for word in range(1, len(arguments)):
-        userId1 = userId1 + arguments[word] + " "
-    userId1 = userId1[0:-1]
-
+    for i in range(1, len(arguments)):
+        userId1 = userId1 + arguments[i] + " "
+    userId1 = userId1[:-1]
     exists1 = False
+
     userId2 = input()
     exists2 = False
 
@@ -156,9 +161,9 @@ def addFriend(arguments):
         print(userId1 + " cannot be the same as " + userId2 + "!")
         return
     if user1 in user2.getFriends():
-        print(userId1 + " must really admire " + userId2)
+        print(userId1 + " must really admire " + userId2+ "!")
         return
-    
+
     user1.addFriend(user2)
     user2.addFriend(user1)
     print(userId1 + " is friend of " + userId2 + ".")
@@ -197,30 +202,28 @@ def friends(arguments):
 def post(arguments):
 
     userId = ""
-    for word in range(1, len(arguments)):
-        userId = userId + arguments[word] + " "
-    userId = userId[0:-1]
+    for i in range(1, len(arguments)):
+        userId += arguments[i] + " "
+    userId = userId[:-1]
     exists = False
-   
-    line = input()
-    hashtags = line.split()
-    
-    
-    line = input()
-    lastinput = line.split()
-    HF = lastinput[0] # honest or fake
-    
-    message = ""
-    for i in range(1, len(lastinput)):
-        message = message + lastinput[i] + " "
 
-    message[0:-1]
-  
     users = fakebook.getUsers()
+
     for u in users:
         if userId == u.getId():
-            exists = True
             user = u
+            exists = True
+
+
+    hashtags = input().split()
+    lastInput = input().split()
+    stance = lastInput[0]
+
+    message = ""
+    for i in range(1, len(lastInput)):
+        message += lastInput[i] + " "
+    message = message[:-1]
+
     if exists == False:
         print(userId + " does not exist!")
         return
@@ -236,20 +239,19 @@ def post(arguments):
         if counter > 1:
             print("Invalid hashtags list!")
             return
- 
-    if user.getUserKind().upper() == "FANATIC":
-        hates = user.getHates()
-        for i in hashtags:
-            for j in hates:
-                if i == j and HF == "honest":
-                    print("Inadequate stance!")
-                    return
+
+    if user.getUserKind().upper() == "FANATIC" and stance.upper() == "HONEST":
+        lovesHates = user.getLovesHates()
+        for entry in lovesHates:
+            if entry[0] == "HATES" and entry[1] in hashtags:
+                print("inadequate stance!")
+                return
     
-    post = Post(user, hashtags, HF, message)
+    post = Post(user, hashtags, stance, message)
     fakebook.addPost(post)
     user.addPost(post)
     friends = user.getFriends()
-    print(userId + " sent a " + HF + " post to " + str(len(friends)) + " friends. Post id = " + str(len(user.getPosts())))
+    print(userId + " sent a " + stance + " post to " + str(len(friends)) + " friends. Post id = " + str(len(user.getPosts())) + ".")
 
 def userPosts(arguments):
 
@@ -278,29 +280,39 @@ def userPosts(arguments):
     counter = 1
     print(userId + " posts:")
     for post in posts:
-        HF = post.getHF()
+        stance = post.getStance()
         message = post.getMessage()
         numberComments = len(post.getComments())
         
-        print("{}. [{}] {}[{} comments]".format(counter, HF, message, numberComments))
-         
+        print("{}. [{}] {} [{} comments]".format(counter, stance, message, numberComments))
+
 def comment(arguments):
 
     userId1 = ""
     for word in range(1, len(arguments)):
-        userId1 = userId1 + arguments[word] + " "
+        userId1 += arguments[word] + " "
     userId1 = userId1[0:-1]
     exists1 = False
 
-    users = fakebook.getUsers()
+    userId2 = input()
+    exists2 = False
 
+    lastInput = input().split()
+
+    postId = int(lastInput[0])
+    commentStance = lastInput[1]
+
+    message = ""
+    for word in range(2, len(lastInput)):
+        message += lastInput[word] + " "
+    message = message[0:-1]
+
+    users = fakebook.getUsers()
     for u in users:
         if userId1 == u.getId():
             exists1 = True
             user1 = u
 
-    userId2 = input()
-    exists2 = False
     for u in users:
         if userId2 == u.getId():
             exists2 = True
@@ -309,13 +321,49 @@ def comment(arguments):
     if exists1 == False:
         print(userId1 + " does not exist!")
         return
-
     if exists2 == False:
         print(userId2 + " does not exist!")
         return
 
+    postList = user2.getPosts()
+
+    if postId > len(postList):
+        print("{} has no post {}!".format(userId2, postId))
+        return
+
+    hasAccess = False
+    friends2 = user2.getFriends()
+    friends2.append(user2) # o user2 pode comentar no seu proprio post
+    for u in friends2:
+        if user1 == u:
+            hasAccess = True
+
+    if hasAccess == False:
+        print("{} has no access to post {} by {}!".format(userId1, postId, userId2))
+        return 
+
+    if user1.getUserKind().upper() == "SELFCENTERED" and user1 == user2:
+        print(userId1 + " cannot comment on this post!")
+        return
 
     
+    postStance = post.getStance()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
