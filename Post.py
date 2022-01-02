2eb18692361afd4364d0551b46da2e3941e175a7
mario_santos <75098183+mario-santosgithub@users.noreply.github.com>
2022-01-02 23:17:33 +0000

    update Post.py

--- Post.py
+++ Post.py
@@ -1,10 +1,11 @@
 class Post:
 
-    def __init__(self, user, hashtags, stance, message):
+    def __init__(self, user, hashtags, stance, postId, message):
         self.user = user
         self.hashtags = hashtags
         self.stance = stance
         self.message = message
+        self.postId = postId
         self.comments = []
 
     def getUser(self):
@@ -21,6 +22,9 @@ class Post:
     
     def getComments(self):
         return self.comments
+
+    def getId(self):
+        return self.postId
 
     def addComment(self, comment):
         self.comments.append(comment)
